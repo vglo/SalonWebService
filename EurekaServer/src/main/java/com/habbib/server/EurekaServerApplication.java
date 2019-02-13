96c489add5ac0ef8c4a2459c24284451480a0b04
yash <yash@Agrawal-Y>
2019-02-13 22:57:25 +0530

    New Eureka server project wtih zuul proxy

---
+++ EurekaServer/src/main/java/com/habbib/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.habbib.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
+
