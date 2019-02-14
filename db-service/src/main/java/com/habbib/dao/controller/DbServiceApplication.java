9942e49cb81a58a9a16d206ffc593c7a03ce4b20
yash <yash@Agrawal-Y>
2019-02-14 13:33:21 +0530

    Databse module

---
+++ db-service/src/main/java/com/habbib/dao/controller/DbServiceApplication.java
@@ -0,0 +1,14 @@
+package com.habbib.dao.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServiceApplication.class, args);
+	}
+
+}
+
