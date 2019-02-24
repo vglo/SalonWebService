1c510c1656d3253b4769d7de748b79d54da8e463
Vaibhav Lokhande <vaibhavglokhande@gmail.com>
2019-02-24 21:01:21 +0530

    Enable CORS support

--- EurekaServer/src/main/java/com/habbib/server/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/habbib/server/EurekaServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,6 +26,22 @@ public class EurekaServerApplication {
 	UiConfiguration uiConfig() {
 		return new UiConfiguration("validatorUrl", "list", "alpha", "schema",
 				UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
 	}
 
 }
